9537bf330d44cbbb8bc2dc90701bbb777b7edd50
hyingzi <h.yingzi@gmail.com>
2017-03-24 14:33:14 +0800

    react build HMR

---
+++ src/component/tips/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tips = () => (
+  <div className={styles.main}>
+    <h2>Digiocean</h2>
+  </div>
+)
+
+export default Tips
---
+++ src/container/app/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './app.css'
+import Tips from '../../component/tips'
+
+const App = () => (
+  <div className={styles.main}>
+    <h2>Digiocean</h2>
+  </div>
+)
+
+export default App
